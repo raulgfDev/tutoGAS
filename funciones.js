c6955dbd3ac07aa7268719b0d38ef35d6f5cfc90
raul <raulgf1982@gmail.com>
2024-10-11 10:23:35 +0200

    v0

---
+++ funciones.js
@@ -0,0 +1,17 @@
+
+// no se si funcionan arrow func. entonces sntx normal
+function doGet() {
+
+    let indexHTML = HtmlService.createTemplateFromFile('index');
+    indexHTML.datos = {
+        data1: 'desde el back al front',
+        data2: 'esto es data2'
+    }
+
+    return indexHTML.evaluate().setTitle('AgendaGAS');
+}
+
+
+function obtenerDatosHTML ( fichero ) {
+    return HtmlService.createHtmlOutputFromFile( fichero ).getContent();
+}
